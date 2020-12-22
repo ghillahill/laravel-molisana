82b50346ceb2d57ae53fd5922d460e154b38d2a1
Alberto <thebertsinfo@gmail.com>
2020-12-22 17:55:49 +0100

    Stampo pasta per tipo in modo dinamico

--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -1,14 +1,36 @@
 @extends('layouts.app')
 
-@section('content')
-    <h1>Prodotti</h1>
-@endsection
 @section('cards')
-    @foreach ($pasta as $item)
-        <div class="cards">
-            <img src="{{$item['src']}}" alt="pasta">
-        </div>
-    @endforeach
+    <h2>LE LUNGHE</h2>
+    <div class="cards-box">
+        @foreach ($pasta as $item)
+            @if($item['tipo'] === 'lunga')
+            <div class="card">
+                <img src="{{$item['src']}}" alt="pasta">
+            </div>
+            @endif
+        @endforeach
+    </div>
+    <h2>LE CORTE</h2>
+    <div class="cards-box">
+        @foreach ($pasta as $item)
+            @if($item['tipo'] === 'corta')
+            <div class="card">
+                <img src="{{$item['src']}}" alt="pasta">
+            </div>
+            @endif
+        @endforeach
+    </div>
+    <h2>LE CORTISSIME</h2>
+    <div class="cards-box">
+        @foreach ($pasta as $item)
+            @if($item['tipo'] === 'cortissima')
+            <div class="card">
+                <img src="{{$item['src']}}" alt="pasta">
+            </div>
+            @endif
+        @endforeach
+    </div>
 @endsection
 
 @section('page-title', 'Our Products - Molisana')
